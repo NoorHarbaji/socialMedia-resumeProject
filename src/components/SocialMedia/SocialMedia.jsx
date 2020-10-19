aec3021ba7888ef74926fa3a636f9760efae90c6
Noor <noorharbaji@outlook.sa>
2020-10-19 13:48:22 +0200

    initial commit

--- src/components/SocialMedia/SocialMedia.jsx
+++ src/components/SocialMedia/SocialMedia.jsx
@@ -3,14 +3,12 @@ import "./style.css";
 import { classStyle } from "./SocialListFun";
 
 const SocialMedia = ({ socailList }) => {
-  const socailMediaList = socailList.map(item => (
-    <>
-      <li key={item.name}>
-        <a href={item.url} target="_blank" rel="noopener noreferrer">
-          {classStyle(item.name)}
-        </a>
-      </li>
-    </>
+  const socailMediaList = socailList.map((item) => (
+    <li key={item.name}>
+      <a href={item.url} target="_blank" rel="noopener noreferrer">
+        {classStyle(item.name)}
+      </a>
+    </li>
   ));
   return <ul>{socailMediaList}</ul>;
 };
